bdda16c14e5331b666136f4d2c5d5e4c5dab2777
Medgyesi Zsolt <zmedgyes@gmail.com>
2016-02-12 16:18:23 +0100

    first upload

---
+++ FieldOfView/Assets/Scripts/TestPath.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPath : MonoBehaviour {
+    public Grid grid;
+
+	void Update () {
+        Node node = grid.NodeFromWorldPoint(transform.position);
+        node.walkable = false;
+	}
+}
---
+++ rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+        float hmov = Input.GetAxis("Horizontal");
+        float vmov = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(0.0f, 0.0f, vmov);
+        rb.MovePosition(rb.position + mov.normalized * speed);
+        if (vmov < 0) { transform.Rotate(new Vector3(0.0f, -hmov, 0.0f)); }
+        else { transform.Rotate(new Vector3(0.0f, hmov, 0.0f)); }
+       // transform.Rotate(new Vector3(0.0f,hmov,0.0f));
+       // rb.AddForce(mov * speed);
+    }
+}
